33480ff08d4d87f690c4870469dc0b2d8bbdfa30
Caleb Voorhees <cvoorhees44@gmail.com>
2024-08-17 13:58:19 -0400

    Removed "Skills:" from skill list

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
             <p className="text-md py-3 px-6 max-w-5xl mx-auto leading-8 text-gray-800 dark:text-gray-500">
               Software engineer at C Speed with a strong desire to contribute to software solutions that help people. Actively developing, testing, and maintaining embedded software for industry leading medical equipment.            </p>
             <p className="text-md py-3 mx-auto leading-8 font-bold text-gray-800 dark:text-gray-500">
-              Skills: C++, C, C#, Python, Swift, PHP, SQL, Bash, Qt, .NET, XAML, Markdown, HTML, Tailwind CSS, SwiftUI, Visual Studio, Xcode, VSCode, Git, Jira, Azure DevOps, Jenkins, Vercel, VirtualBox
+              C++, C, C#, Python, Swift, PHP, SQL, Bash, Qt, .NET, XAML, Markdown, HTML, Tailwind CSS, SwiftUI, Visual Studio, Xcode, VSCode, Git, Jira, Azure DevOps, Jenkins, Vercel, VirtualBox
             </p>
           </div>
           <div class= "flex flex-row justify-center">
